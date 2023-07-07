ba0f3c5755732fbe5eb90119200c13276cb0451c
Tatyana <quininthenorth@gmail.com>
2023-07-07 14:55:45 +0400

    DbStore

---
+++ src/main/java/ru/job4j/dream/store/MainStore.java
@@ -0,0 +1,30 @@
+package ru.job4j.dream.store;
+
+import ru.job4j.dream.model.Candidate;
+import ru.job4j.dream.model.Post;
+
+public class MainStore {
+    public static void main(String[] args) {
+        Store store = DbStore.instOf();
+        var p = new Post(0, "Java Job");
+        store.save(p);
+        for (Post post : store.findAllPosts()) {
+            System.out.println(post.getId() + " " + post.getName());
+        }
+        store.save(new Post(p.getId(), "Java Job Junior"));
+        for (Post post : store.findAllPosts()) {
+            System.out.println(post.getId() + " " + post.getName());
+        }
+        System.out.println(store.findById(p.getId()).getName());
+        var c = new Candidate(0, "Java Developer");
+        store.save(c);
+        for (Candidate candidate : store.findAllCandidates()) {
+            System.out.println(candidate.getId() + " " + candidate.getName());
+        }
+        store.save(new Candidate(c.getId(), "Java Junior Developer"));
+        for (Candidate candidate : store.findAllCandidates()) {
+            System.out.println(candidate.getId() + " " + candidate.getName());
+        }
+        System.out.println(store.findCandidateById(c.getId()).getName());
+    }
+}
